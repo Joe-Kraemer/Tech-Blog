6ebb1f6357c09c94a800e98fe13e66cd6004c955
Joe Kraemer <joekraemer149@gmail.com>
2023-06-06 10:57:57 -0400

    I am committing

---
+++ public/js/new.js
@@ -0,0 +1,27 @@
+const editPostFormhandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const title = document.querySelector('#title').value.trim();
+    const article = document.querySelector('#article').value.trim();
+  const id = document.querySelector("#id").innerHTML
+    if (title && article) {
+      // Send a POST request to the API endpoint
+      const response = await fetch(`/api/posts/post`, {
+        method: 'POST',
+        body: JSON.stringify({ title, article }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.post-form')
+    .addEventListener('submit', editPostFormHandler);
